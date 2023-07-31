11f1ff1c586bca7784851fb33154857db742dc1b
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-07-30 23:37:27 -0600

    Uploading Blender´s models from .obj files.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <SDL2/SDL.h>
-#include <cstdint>
 #include <ctime>
 #include "glm/glm.hpp"
 #include <iostream>
 #include <vector>
 #include <array>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
@@ -18,6 +20,12 @@ Color clearColor = {0, 0, 0, 255};
 Color currentColor = {255, 255, 255, 255};
 
 SDL_Renderer* renderer;
+
+struct Face {
+    std::array<int, 3> vertexIndices;
+};
+
+
 
 void point(int x, int y) {
     SDL_RenderDrawPoint(renderer, x, y);
@@ -34,57 +42,117 @@ void triangle(const glm::vec3& A, const glm::vec3& B, const glm::vec3& C) {
     line(C, A);
 }
 
-std::vector<glm::vec3> vertices = {
-        {1.125986f, 0.781798f, -1.058748f},
-        {0.074502f, -0.860990f, -1.501048f},
-        {1.568286f, 0.015753f, 0.735004f},
-        {0.516802f, -1.627034f, 0.292704f},
-        {-0.516802f, 1.627034f, -0.292704f},
-        {-1.568286f, -0.015753f, -0.735004f},
-        {-0.074502f, 0.860990f, 1.501048f},
-        {-1.125986f, -0.781798f, 1.058748f}
-};
+void render(const std::vector<glm::vec3>& vertexArray) {
+    // Limpiar la pantalla antes de dibujar
+    SDL_SetRenderDrawColor(renderer, clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+    SDL_RenderClear(renderer);
 
-std::vector<std::array<int, 3>> faces = {
-        { {5, 3, 1} },
-        { {3, 8, 4} },
-        { {7, 6, 8} },
-        { {2, 8, 6} },
-        { {1, 4, 2} },
-        { {5, 2, 6} },
-        { {5, 7, 3} },
-        { {3, 7, 8} },
-        { {7, 5, 6} },
-        { {2, 4, 8} },
-        { {1, 3, 4} },
-        { {5, 1, 2} }
-};
+    // Establecer el color actual para dibujar los triángulos
+    SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
 
-void render() {
-    glm::vec3 projection(400.0f, 400.0f, 0.0f);
-    float scale = 100.0f;
+    // Dibujar cada triángulo con las coordenadas proyectadas
+    for (size_t i = 0; i < vertexArray.size(); i += 3) {
+        const glm::vec3& A = vertexArray[i];
+        const glm::vec3& B = vertexArray[i + 1];
+        const glm::vec3& C = vertexArray[i + 2];
 
-    for (const auto& face : faces) {
-        const glm::vec3& A = vertices[face[0] - 1];
-        const glm::vec3& B = vertices[face[1] - 1];
-        const glm::vec3& C = vertices[face[2] - 1];
+        // Aplicar el offset para que el modelo se centre en la ventana
+        int offsetX = WINDOW_WIDTH / 2;
+        int offsetY = WINDOW_HEIGHT / 2;
 
-        glm::vec3 A_projected = projection + A * scale;
-        glm::vec3 B_projected = projection + B * scale;
-        glm::vec3 C_projected = projection + C * scale;
-
-        triangle(A_projected, B_projected, C_projected);
+        // Dibujar los triángulos sumando el offset
+        triangle(A + glm::vec3(offsetX, offsetY, 0), B + glm::vec3(offsetX, offsetY, 0), C + glm::vec3(offsetX, offsetY, 0));
     }
 }
 
-Color getRandomColor() {
-    return {static_cast<uint8_t>(rand() % 256),
-            static_cast<uint8_t>(rand() % 256),
-            static_cast<uint8_t>(rand() % 256),
-            255};
+std::vector<glm::vec3> setupVertexArray(const std::vector<glm::vec3>& vertices, const std::vector<Face>& faces)
+{
+    std::vector<glm::vec3> vertexArray;
+
+    // Ajusta esta escala manualmente para obtener el tamaño deseado del modelo en la ventana
+    float scale = 30.0f;
+
+    // For each face
+    // For each face
+    for (const auto& face : faces)
+    {
+        // Get the vertex positions from the input arrays using the indices from the face
+        glm::vec3 vertexPosition1 = vertices[face.vertexIndices[0]];
+        glm::vec3 vertexPosition2 = vertices[face.vertexIndices[1]];
+        glm::vec3 vertexPosition3 = vertices[face.vertexIndices[2]];
+
+        // Scale the vertex positions
+        glm::vec3 vertexScaled1 = vertexPosition1 * scale;
+        glm::vec3 vertexScaled2 = vertexPosition2 * scale;
+        glm::vec3 vertexScaled3 = vertexPosition3 * scale;
+
+        // Add the vertex positions to the vertex array
+        vertexArray.push_back(vertexScaled1);
+        vertexArray.push_back(vertexScaled2);
+        vertexArray.push_back(vertexScaled3);
+    }
+
+    return vertexArray;
+}
+
+bool loadOBJ(const std::string& path, std::vector<glm::vec3>& out_vertices, std::vector<Face>& out_faces) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open OBJ file: " << path << std::endl;
+        return false;
+    }
+
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<std::array<int, 3>> temp_faces;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string type;
+        iss >> type;
+
+        if (type == "v") {
+            glm::vec3 vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+            temp_vertices.push_back(vertex);
+            } else if (type == "f") {
+            std::array<int, 3> face_indices{};
+            for (int i = 0; i < 3; i++) {
+                std::string faceIndexStr;
+                iss >> faceIndexStr;
+
+                // Find the position of the first slash to extract the vertex index
+                size_t pos = faceIndexStr.find_first_of('/');
+                if (pos != std::string::npos) {
+                    faceIndexStr = faceIndexStr.substr(0, pos);
+                }
+
+                face_indices[i] = std::stoi(faceIndexStr); // No restar 1
+            }
+            temp_faces.push_back(face_indices);
+        }
+    }
+
+    out_vertices = std::move(temp_vertices);
+
+    // Convert std::array<int, 3> to std::vector<std::array<int, 3>>
+    out_faces.reserve(temp_faces.size() * 2); // Reserve space for triangles
+    for (const auto& face : temp_faces) {
+        Face f1{}, f2{};
+
+        // Triangulate the face (split the square into two triangles)
+        f1.vertexIndices = { face[0] - 1, face[1] - 1, face[2] - 1 };
+        f2.vertexIndices = { face[0] - 1, face[2] - 1, face[3] - 1 };
+
+        out_faces.push_back(f1);
+        out_faces.push_back(f2);
+    }
+
+    return true;
 }
 
 int main(int argc, char* argv[]) {
+
     SDL_Init(SDL_INIT_EVERYTHING);
 
     srand(time(nullptr));
@@ -94,6 +162,17 @@ int main(int argc, char* argv[]) {
 
     int renderWidth, renderHeight;
     SDL_GetRendererOutputSize(renderer, &renderWidth, &renderHeight);
+
+    std::vector<glm::vec3> vertices;
+    std::vector<Face> faces;
+
+    bool success = loadOBJ("../Nave.obj", vertices, faces);
+    if (!success) {
+        // Manejo del error si la carga del archivo falla
+        return 1;
+    }
+
+    std::vector<glm::vec3> vertexArray = setupVertexArray(vertices, faces);
 
     bool running = true;
     SDL_Event event;
@@ -106,9 +185,9 @@ int main(int argc, char* argv[]) {
         }
         SDL_SetRenderDrawColor(renderer, clearColor.r, clearColor.g, clearColor.b, clearColor.a);
         SDL_RenderClear(renderer);
+        // Llamada a la función render con la matriz de vértices transformados
         SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
-
-        render(); // Llamada a la función render para mostrar el cubo triangulado
+        render(vertexArray); // Solo se pasa la matriz de vértices transformados
         SDL_RenderPresent(renderer);
         SDL_Delay(1000 / 60);
     }
